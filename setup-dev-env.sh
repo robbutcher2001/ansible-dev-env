d690eb9d74b75fa22e08d44f147bef95f07583dd
Rob Butcher <rob.butcher2@baesystems.com>
2018-04-30 17:13:25 +0100

    Installing Kubernetes

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# System updates
+sudo apt-get update
+
+# Git
+sudo apt install -y git
+
+# Ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
 GIT_REPO=$(git config --get remote.origin.url)
 sudo ansible-pull -U $GIT_REPO
