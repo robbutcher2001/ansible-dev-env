0915152b4bc37340492753046ee7d08324da1232
Rob Butcher <rob.butcher2@baesystems.com>
2018-05-02 12:49:40 +0100

    Removing prompt

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -8,7 +8,7 @@ sudo apt install -y git
 
 # Ansible
 sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
